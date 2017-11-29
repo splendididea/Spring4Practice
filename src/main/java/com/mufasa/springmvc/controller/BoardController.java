fe3e50a16b9f38f53c3d010d38bee2a7e8410066
splendididea <splendid_idea@naver.com>
2017-11-30 01:01:05 +0900

    list 페이지 c taglib 오류 수정 가져오는 uri변경
    read Controller 추가

--- src/main/java/com/mufasa/springmvc/controller/BoardController.java
+++ src/main/java/com/mufasa/springmvc/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.inject.Inject;
@@ -41,5 +42,10 @@ public class BoardController {
     public void listAll(Model model) throws Exception{
         logger.info("in listAll..........");
         model.addAttribute("list" , boardService.listAll());
+    }
+
+    @RequestMapping(value = "read", method = RequestMethod.GET)
+    public void read(@RequestParam("bno")int bno, Model model) throws Exception{
+        model.addAttribute(boardService.read(bno));
     }
 }
